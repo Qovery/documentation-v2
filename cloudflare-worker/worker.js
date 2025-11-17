1fc4582cdaade8f81077ae3860326645626db125
Julien Dan <juliendan@mac.home>
2025-11-17 11:48:34 +0100

    Add IAM AWS Qovery policy

--- cloudflare-worker/worker.js
+++ cloudflare-worker/worker.js
@@ -8,6 +8,84 @@ export default {
         return await fetch(request);
       }
 
+      // Serve the qovery-iam-aws.json file directly
+      if (urlObject.pathname === '/docs/files/qovery-iam-aws.json') {
+        const iamPolicy = {
+          "Version": "2012-10-17",
+          "Statement": [
+            {
+              "Effect": "Allow",
+              "Action": [
+                "iam:*",
+                "s3:ListAllMyBuckets",
+                "cloudwatch:*",
+                "autoscaling:*",
+                "application-autoscaling:*",
+                "elasticloadbalancing:*",
+                "organizations:DescribeAccount",
+                "organizations:DescribeOrganization",
+                "organizations:DescribeOrganizationalUnit",
+                "organizations:DescribePolicy",
+                "organizations:ListChildren",
+                "organizations:ListParents",
+                "organizations:ListPoliciesForTarget",
+                "organizations:ListRoots",
+                "organizations:ListPolicies",
+                "organizations:ListTargetsForPolicy",
+                "dynamodb:*",
+                "ecr:*",
+                "ec2:*",
+                "elasticache:*",
+                "cloudtrail:LookupEvents",
+                "dynamodb:*",
+                "tag:GetResources",
+                "rds:*",
+                "ecs:*",
+                "eks:*",
+                "logs:*",
+                "events:DescribeRule",
+                "events:DeleteRule",
+                "events:ListRuleNamesByTarget",
+                "events:ListTargetsByRule",
+                "events:PutRule",
+                "events:PutTargets",
+                "es:AddTags",
+                "es:RemoveTags",
+                "es:ListTags",
+                "es:DeleteElasticsearchDomain",
+                "es:DescribeElasticsearchDomain",
+                "es:CreateElasticsearchDomain",
+                "events:RemoveTargets",
+                "kms:*",
+                "events:TagResource",
+                "events:ListTagsForResource"
+              ],
+              "Resource": "*"
+            },
+            {
+              "Action": [
+                "s3:*",
+                "sqs:*"
+              ],
+              "Effect": "Allow",
+              "Resource": [
+                "arn:aws:s3:::qovery*",
+                "arn:aws:s3:::qovery*/*",
+                "arn:aws:sqs:*:*:qovery*",
+                "arn:aws:sqs:*:*:qovery*/*"
+              ]
+            }
+          ]
+        };
+
+        return new Response(JSON.stringify(iamPolicy, null, 2), {
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          }
+        });
+      }
+
       // If the request is to /docs or /docs/, redirect to /docs/introduction
       if (urlObject.pathname === '/docs' || urlObject.pathname === '/docs/') {
         return Response.redirect(urlObject.origin + '/docs/getting-started/introduction', 301);
